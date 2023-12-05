d66ed4a83ef6473e3bc5773a5b33ae3577852e03
G N W Gunasekara <90319050+Winter-G@users.noreply.github.com>
2023-12-05 22:00:38 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-// Entry point of the application, rendering the main navigator within a navigation container.
-
+// Importing necessary components from 'expo-status-bar' and 'react-native'.
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+
+// Importing 'NavigationContainer' from '@react-navigation/native'.
+import { NavigationContainer } from '@react-navigation/native';
+
+// Importing the main navigator component from './Navigator'.
 import Navigator from './Navigator';
 
-
+// Entry point of the application, rendering the main navigator within a navigation container.
 export default function App() {
   return (
-    
-      <Navigator/>
-    
+    // Wrapping the main navigator with 'NavigationContainer'.
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
   );
 }
 
+// Styles for the 'container' component.
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -22,3 +27,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
